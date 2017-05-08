e27cbff1456ab1e9772dd0b8ddbf400d525cb700
Saurabh Kanhegaonkar <saurabh.kanhegaonkar@gmail.com>
2017-05-08 23:57:01 +0530

    Initial labels and layout done

--- pomodoro/src/App.js
+++ pomodoro/src/App.js
@@ -4,11 +4,22 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className = "panel panel-default">
-        <div className = "panel panel-body">
-        Hello World
+        <div className="panel panel-default app-content center-block">
+          <div className="panel panel-body ">
+            <div className="container-fluid">
+              <div className="row">
+                <h2 className="text-center">Pomodoro Timer</h2>
+              </div>
+              <div className="row">
+                <h2 className="text-center">00:00:00</h2>
+              </div>
+              <div className="row">
+                <h3 className="text-primary">Set Timer</h3>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
+
     );
   }
 }
