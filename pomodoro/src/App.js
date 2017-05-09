fd8456152099961909cbd42ea3df3f95afcae7f9
Saurabh Kanhegaonkar <saurabh.kanhegaonkar@gmail.com>
2017-05-09 11:00:41 +0530

    html outlining with bootstrap done. DOM ready

--- pomodoro/src/App.js
+++ pomodoro/src/App.js
@@ -14,7 +14,40 @@ class App extends Component {
                 <h2 className="text-center">00:00:00</h2>
               </div>
               <div className="row">
+                <button className="btn btn-success center-block">Start</button>
+              </div>
+              <div className="row">
                 <h3 className="text-primary">Set Timer</h3>
+                <div className="row control-row">
+                  <div className="form-group">
+                    <div className="col-sm-3">
+                      <label htmlFor="hours"> Hours </label>
+                    </div>
+                    <div className="col-sm-9">
+                      <input className="form-control" type="number" id="hours" />
+                    </div>
+                  </div>
+                </div>
+                <div className="row control-row">
+                  <div className="form-group">
+                    <div className="col-sm-3">
+                      <label htmlFor="minutes">Minutes</label>
+                    </div>
+                    <div className="col-sm-9">
+                      <input className="form-control" type="number" id="minutes" />
+                    </div>
+                  </div>
+                </div>
+                <div className="row control-row">
+                  <div className="form-group">
+                    <div className="col-sm-3">
+                      <label htmlFor="seconds"> Seconds </label>
+                    </div>
+                    <div className="col-sm-9">
+                      <input className="form-control" type="number" id="seconds" />
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
