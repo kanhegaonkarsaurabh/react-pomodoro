631e74d63dc327403ff1e3c69a9d0565e50249a7
Saurabh Kanhegaonkar <saurabh.kanhegaonkar@gmail.com>
2017-05-10 15:21:09 +0530

    moment.js installed

--- pomodoro/src/Timer/components/timer.js
+++ pomodoro/src/Timer/components/timer.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import TimerHeader from '../../TimerHeader/components/timerheader';
 import TimerDisplay from '../../TimerDisplay/components/timerdisplay';
 import TimerButton from '../../TimerButton';
 import TimerConfig from '../../TimerConfig/components/timerconfig';
-const Timer = () =>
-   (
-    <div className="container-fluid">
-      <TimerHeader />
-      <TimerDisplay />
-      <TimerButton />
-      <TimerConfig />
-    </div>
-  );
-
+class Timer extends Component {
+  render() {
+     return (
+      <div className="container-fluid">
+        <TimerHeader />
+        <TimerDisplay />
+        <TimerButton />
+        <TimerConfig />
+      </div>
+     );
+  }
+}
 export default Timer;
